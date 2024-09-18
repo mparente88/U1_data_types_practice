6ddd5bda879c30ad76ac17e129f765b069f328aa
mparente88 <parente.molly@gmail.com>
2024-09-18 13:24:32 -0700

    complete

--- script.js
+++ script.js
@@ -4,12 +4,23 @@
  * Declare a variable called myFavoriteSong on one line and console.log the value
  * Then, using your Literals, console.log "my favorite song is..." with the song's value
  */
+let myFavoriteSong = "New Born, by Muse"
+
+//I hope I'm not the only one that likes Muse here. 
+//I saw them in concert two years ago and it was phenomenal.
+//Also, I set it as let because my favorite song shifts about twice an hour.
+
+console.log(`My favorite song is... ` + myFavoriteSong)
 
 /*
  * Prompt 2:
  *
  * Create 3 variables using let and const, then console.log each of them, using a string literal (` ${} `} in the log
  */
+
+console.log(`My favorite song is... ${myFavoriteSong}`)
+
+//I love these inline strings.
 
 /*
  * Prompt 3:
@@ -21,6 +32,17 @@
  *   - divide your variable by 7
  *   - calculate the remainder of dividing your variable by 2
  */
+
+let myNumber = 10
+
+console.log(myNumber + 1)
+console.log(myNumber - 1)
+console.log(myNumber / 2)
+console.log(myNumber * 2)
+console.log(myNumber % 3)
+
+//I really enjoy the "this is how this might be used in developing a video game" examples
+//that you give, such as for why we might use the remainder.
 
 /*
  * Prompt 4:
@@ -34,12 +56,36 @@
  * Run a console.log that says 'hello my name is..." with your fullName. 
  */
 
+let firstName = "Molly"
+
+let lastName = "Parente"
+
+let fullName = `${firstName} ${lastName}`
+
+console.log(`hello my name is... ${fullName}`)
 
 /* Arrays */
 
 // Create an array of Movies, then log the 2nd movie in your array
 // Use your array methods to add a movie in to the Front of the array, then the Back of the Array, then to the 2nd index point of the array
 
+let madMaxMovies = [
+  'Mad Max',
+  'Mad Max: The Road Warrior',
+  'Mad Max: Beyond Thunderdome',
+  'Mad Max: Fury Road',
+  'Furiosa: A Mad Max Saga',
+]
+
+madMaxMovies.unshift(`Mad Max: The Prequel`)
+
+madMaxMovies.push('Nux: A Mad Max Saga')
+
+//I had to look up how to index something into the 2nd spot, but the MDN helped.
+
+madMaxMovies.splice(1, 0, `Mad Max: The Prequel Sequel`)
+
+console.log(madMaxMovies)
 
 /*
  *
@@ -57,12 +103,42 @@ let array3 = [
   ['Marty Stuart', ['Marty Janetty', ['Marty McFly'], 'Marty Robbins']]
 ];
 
+console.log(array1[2])
+
+console.log(array2.indexOf(`Marty McFly`))
+
+
+//I used the MDN to learn about indexOf and was getting -1 back, which means that it doesn't
+//exist. I know it does exist, but I'm not getting how to log it independent of Marty Stuart.
+
+console.log(array2[1][1])
+
+//Okay, then I tried the thing that I did in the first place, but it worked this time.
+//I must've had a typo. We're all good now!
+
+console.log(array3[1][1][1])
+
+//I nested the square brackets at first, but that didn't work. Trying it this way returns
+//an array, not a string. Am I supposed to convert it into a string?
+
+console.log(array3[1][1][1][0])
+
+// Wait. Oh, duh. Just slap another one on there.
+
 /*
  *
  * Use the length of the array below to retrieve the second to last item.
  */
 
 let array4 = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(array4[3])
+
+let lookingForLetter = array4.length - 2
+
+console.log(array4[lookingForLetter])
+
+//Ashley helped me through this one.
 
 /*
  *
@@ -83,3 +159,61 @@ let cathleen = ['Cathleen', 186, 'New York'];
 // Change Cathleen's hometown from New York to "Gotham City".
 
 // Remove "Pittsburgh" from Kristyn's array and add "Oakland".
+
+thom.shift()
+
+thom.unshift(`Tom`)
+
+console.log(thom)
+
+karolin.splice(1, 1, 17)
+
+console.log(karolin)
+
+cathleen.pop()
+cathleen.push(`Gotham City`)
+
+console.log(cathleen)
+
+kristyn.pop()
+kristyn.push(`Oakland`)
+
+console.log(kristyn)
+
+
+//Ashley challenged me to make a text-based if/then adventure game, so I'm going to
+//putz around with that a little.
+
+heroName = "Sir Jeremy" //Enter any name
+heroPronoun = "he" //Enter "he" "she" or "they", so that the game's narration can appropriately refer to the hero
+heroPronoun2 = "him" //Enter "him" "her" "them"
+heroPossessive = "his" //enter "his" "hers" "theirs"
+heroClass = "fighter" //Enter "fighter" "thief" or "wizard"
+
+basicInventory = [
+  `a backpack`,
+  'a bedroll',
+  'a torch',
+]
+
+if (heroClass === `fighter`) {
+  basicInventory.unshift(`a steel longsword`)
+  basicInventory.unshift(`a metal shield`)
+  basicInventory.unshift(`a suit of chainmail`)
+  basicInventory.push(`a whetstone`)
+} else if (heroClass === `thief`) {
+  basicInventory.unshift(`a sharp knife`)
+  basicInventory.unshift(`a suit of leather armor`)
+  basicInventory.push(`a bag of caltrops`)
+} else {
+  basicInventory.unshift(`a magic staff`)
+  basicInventory.unshift(`a set of robes`)
+  basicInventory.push(`a focus crystal`)
+}
+
+heroInventory = basicInventory
+
+let myGold = Math.floor((Math.random() * 100))
+
+console.log(`${heroName} brings ${heroInventory.join(', ')}, ${myGold} gold coins and ${heroPossessive} best intentions into the dungeon.`)
+
